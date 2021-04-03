f70413b10d9d003670852e3ebe6810f76e904987
xlzy <1017866168@qq.com>
2021-04-03 17:43:49 +0800

    修复build失败，document is not defined

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,12 +1,17 @@
 // import vue from 'vue/dist/vue.esm.browser'
 import './public/font/fontello.css'
-import APlayer from '@moefe/vue-aplayer'
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   options, // 附加到根实例的一些选项
   router, // 当前应用的路由实例
   siteData // 站点元数据
 }) => {
-  Vue.use(APlayer, { productionTip: false })
+  Vue.mixin({
+    mounted() {
+      import('@moefe/vue-aplayer').then(function (APlayer) {
+        Vue.use(APlayer.default, { productionTip: false })
+      })
+    },
+  })
   // window.Vue = vue // 使页面中可以使用Vue构造函数 （使页面中的vue demo生效）
 }
