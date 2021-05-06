d7f0d3462e96f95590ed27706fc719fbcd39ea5d
xlzy <1017866168@qq.com>
2021-05-06 12:20:30 +0800

    feat: 增加一些收藏夹和文章以及目录的调整

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -2,53 +2,36 @@
 module.exports = [
   { text: '首页', link: '/' },
   {
-    text: '技术',
+    text: '前端',
     link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
+      { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
+      { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
+      { text: 'React', link: '/pages/5d463fbdb172d43b/' },
+      { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
+      { text: 'CSS', link: '/pages/c8f128/'},
+    ],
+  },
+  {
+    text: '编程',
+    items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-      {
-        text: '前端',
-        items: [
-          { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
-          { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
-          { text: 'React', link: '/pages/5d463fbdb172d43b/' },
-          { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
-          { text: 'CSS', link: '/pages/c8f128/'},
-        ],
-      },
-      {
-        text: '前端之外',
-        items: [
-          { text: '技术文档', link: '/pages/9a7ee40fc232253e' },
-          { text: '算法', link: '/pages/dea17d/' },
-        ],
-      },
-      { text: '书单', link: '/pages/c09c43/', }
+      { text: '算法', link: '/pages/dea17d/' },
+      { text: '工具教程', link: '/pages/9a7ee40fc232253e' },
     ],
   },
   {
     text: '面试',
     link: '/interview/',
     items: [
-      {
-        text: '面试题库',
-        link: '/interview/',
-        items: [
-          {text: '面试题清单', link: '/interview/'},
-          {text: 'HTML', link: '/pages/4e46b6/'},
-          {text: 'CSS', link: '/pages/990fc5/'},
-          {text: 'JS', link: '/pages/dcff3f/'},
-          {text: 'Vue', link: '/pages/7a583e/'},
-          {text: 'React', link: '/pages/ccd7a7/'},
-          // {text: '零碎', link: '/pages/a134b2/'}
-        ]
-      },
-      {
-        text: '面试心得',
-        items: [
-          {text: '杂言碎语', link: '/pages/331dbf/'},
-        ]
-      }
+      {text: '面试题清单', link: '/interview/'},
+      {text: 'HTML', link: '/pages/4e46b6/'},
+      {text: 'CSS', link: '/pages/990fc5/'},
+      {text: 'JS', link: '/pages/dcff3f/'},
+      {text: 'Vue', link: '/pages/7a583e/'},
+      {text: 'React', link: '/pages/ccd7a7/'},
+      {text: '零碎', link: '/pages/a134b2/'},
+      {text: '杂言碎语', link: '/pages/331dbf/'},
     ]
   },
   {
@@ -62,7 +45,7 @@ module.exports = [
   },
   {
     text: '收藏',
-    link: '/pages/beb6c0bd8a66cea6/',
+    link: '/collection/',
     items: [
       { text: '前端', link: '/pages/702112/' },
       { text: '工具', link: '/pages/2294fc/' },
@@ -70,6 +53,10 @@ module.exports = [
       { text: '平台', link: '/pages/a14556/' },
       { text: '休闲娱乐', link: '/pages/e2847e/' },
       { text: '社区 & 论坛 & 博客', link: '/pages/a09a50/' },
+      { text: '网站', link: '/pages/66a38c/' },
+      { text: '推荐库', link: '/pages/e8ef9c/' },
+      { text: 'You-Dont-Know', link: '/pages/b1721b/' },
+      { text: '值得一读', link: '/pages/9a7038/' },
     ],
   },
   {
