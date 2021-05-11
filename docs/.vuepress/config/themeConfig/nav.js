a5714d168eb69c610ade961ca9188a36d8fcb80d
xlzy <1017866168@qq.com>
2021-05-11 10:44:17 +0800

    feat: nav

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -6,7 +6,7 @@ module.exports = [
     link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
-      { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
+      { text: 'Vue', link: '/pages/bd1af2f75fd361fc/' },
       { text: 'React', link: '/pages/5d463fbdb172d43b/' },
       { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
       { text: 'CSS', link: '/pages/c8f128/'},
@@ -14,6 +14,7 @@ module.exports = [
   },
   {
     text: '编程',
+    link: '/program',
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
       { text: '算法', link: '/pages/dea17d/' },
@@ -24,7 +25,7 @@ module.exports = [
     text: '面试',
     link: '/interview/',
     items: [
-      {text: '面试题清单', link: '/interview/'},
+      {text: '面试题清单', link: '/interview-list/'},
       {text: 'HTML', link: '/pages/4e46b6/'},
       {text: 'CSS', link: '/pages/990fc5/'},
       {text: 'JS', link: '/pages/dcff3f/'},
